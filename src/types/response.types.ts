5a863b6d8875ea8f57c23744ea2f7b8e9e659829
Mwxyz2022 <rozhukvlad@gmail.com>
2023-07-25 14:01:07 +0300

    fixed types

--- src/types/response.types.ts
+++ src/types/response.types.ts
@@ -1,17 +1,6 @@
-export interface Coord {
+interface Coord {
 	lat: number
 	lon: number
-}
-
-export interface City {
-	id: number
-	name: string
-	coord: Coord
-	country: string
-	population: number
-	timezone: number
-	sunrise: number
-	sunset: number
 }
 
 export interface Temp {
@@ -23,48 +12,33 @@ export interface Temp {
 	morn: number
 }
 
-export interface FeelsLike {
-	day: number
-	night: number
-	eve: number
-	morn: number
-}
-
 export interface WeatherDescription {
 	id: number
 	main: string
-	description: string //
-	icon: string //
+	description: string
+	icon: string
 }
 
-export interface HourlyWeather {
+interface CurrentWeather {
+	dt: number
+	temp: number
+	feels_like: number
+	pressure: number
+	humidity: number
+	dew_point: number
+	visibility: number
+	wind_speed: number
+	wind_deg: number
+	weather: WeatherDescription[]
+	uvi: number
+}
+
+interface HourlyWeather {
 	dt: number
 	temp: number
 }
 
-export interface CurrentWeather {
-	dt: number //
-	sunrise: number
-	sunset: number
-	moonrise: number
-	moonset: number
-	moon_phase: number
-	temp: number //
-	feels_like: number //
-	pressure: number //
-	humidity: number //
-	dew_point: number //
-	visibility: number //
-	wind_speed: number //
-	wind_deg: number //
-	wind_gust: number
-	weather: WeatherDescription[] //
-	clouds: number
-	pop: number
-	uvi: number //
-}
-
-export interface DailyWeather {
+interface DailyWeather {
 	dt: number
 	temp: Temp
 }
@@ -73,7 +47,7 @@ export interface WeatherData {
 	lat: number
 	lon: number
 	timezone: string
-	timezone_offset: number //
+	timezone_offset: number
 	current: CurrentWeather
 	hourly: HourlyWeather[]
 	daily: DailyWeather[]
@@ -82,7 +56,6 @@ export interface WeatherData {
 interface Country {
 	country: string
 }
-
 export interface ICityData {
 	id: number
 	name: string
