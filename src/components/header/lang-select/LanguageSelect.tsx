192860c9bb706eaebfe99fe641589b9383e4252a
Mwxyz2022 <rozhukvlad@gmail.com>
2023-07-28 12:51:13 +0300

    finish fixed

--- src/components/header/lang-select/LanguageSelect.tsx
+++ src/components/header/lang-select/LanguageSelect.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEvent, useRef, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { imageLang } from '../../../data/imageLang'
@@ -20,33 +20,21 @@ const LanguageSelect: FC = () => {
 		localStorage.setItem('language', JSON.stringify(language))
 		setCurrentLang(language)
 		setIsShowAllLang(false)
-
-		if (currentBtnRef.current) {
-			currentBtnRef.current.classList.remove('click')
-		}
 	}
 
 	const onLangBarHandler = () => {
-		setIsShowAllLang(isShowAllLang ? false : true)
-
-		if (currentBtnRef.current && isShowAllLang) {
-			currentBtnRef.current.classList.remove('click')
-		}
+		setIsShowAllLang(prev => !prev)
 	}
 
-	const onMouseUp = (e: MouseEvent<HTMLButtonElement>) => {
-		const isCurrentButton = e.currentTarget.getAttribute('data-name') || ''
-
+	useEffect(() => {
 		if (currentBtnRef.current) {
-			currentBtnRef.current.classList.add('click')
+			if (isShowAllLang) {
+				currentBtnRef.current.classList.remove('click')
+			} else {
+				currentBtnRef.current.classList.add('click')
+			}
 		}
-
-		if (isCurrentButton === 'current') {
-			onLangBarHandler()
-		} else {
-			onSelectLangHandler(isCurrentButton)
-		}
-	}
+	}, [isShowAllLang])
 
 	return (
 		<div
@@ -63,12 +51,11 @@ const LanguageSelect: FC = () => {
 						.map(item => (
 							<button
 								key={item}
-								data-name={item}
 								style={{
 									backgroundImage: `url(${imageLang[item]})`
 								}}
-								className='button__lang button__lang-select'
-								onClick={onMouseUp}
+								className='button__lang button__lang-item'
+								onClick={() => onSelectLangHandler(item)}
 							/>
 						))}
 				</>
@@ -76,15 +63,105 @@ const LanguageSelect: FC = () => {
 
 			<button
 				ref={currentBtnRef}
-				data-name='current'
 				style={{
 					backgroundImage: `url(${imageLang[currentLang]})`
 				}}
 				className='button__lang'
-				onClick={onMouseUp}
+				onClick={onLangBarHandler}
 			/>
 		</div>
 	)
 }
 
 export default LanguageSelect
+
+// import { FC, MouseEvent, useRef, useState } from 'react'
+// import { useTranslation } from 'react-i18next'
+
+// import { imageLang } from '../../../data/imageLang'
+// import { initLanguage } from '../../../utils/local-storage/init-storage'
+
+// import './language-select.css'
+
+// const LanguageSelect: FC = () => {
+// 	const languages = ['ua', 'en']
+
+// 	const [currentLang, setCurrentLang] = useState<string>(initLanguage())
+// 	const [isShowAllLang, setIsShowAllLang] = useState(false)
+// 	const { i18n } = useTranslation()
+
+// 	const currentBtnRef = useRef<HTMLButtonElement>(null)
+
+// 	const onSelectLangHandler = (language: string) => {
+// 		i18n.changeLanguage(language)
+// 		localStorage.setItem('language', JSON.stringify(language))
+// 		setCurrentLang(language)
+// 		setIsShowAllLang(false)
+
+// 		if (currentBtnRef.current) {
+// 			currentBtnRef.current.classList.remove('click')
+// 		}
+// 	}
+
+// 	const onLangBarHandler = () => {
+// 		setIsShowAllLang(prev => !prev)
+
+// 		if (currentBtnRef.current && isShowAllLang) {
+// 			currentBtnRef.current.classList.remove('click')
+// 		}
+// 	}
+
+// 	const onMouseUp = (e: MouseEvent<HTMLButtonElement>) => {
+// 		const isCurrentButton = e.currentTarget.getAttribute('data-name') || ''
+
+// 		if (currentBtnRef.current) {
+// 			currentBtnRef.current.classList.add('click')
+// 		}
+
+// 		if (isCurrentButton === 'current') {
+// 			onLangBarHandler()
+// 		} else {
+// 			onSelectLangHandler(isCurrentButton)
+// 		}
+// 	}
+
+// 	return (
+// 		<div
+// 			className='lang__container'
+// 			style={{
+// 				justifyContent: isShowAllLang ? 'space-evenly' : 'center',
+// 				width: isShowAllLang ? 106 : 56
+// 			}}
+// 		>
+// 			{isShowAllLang && (
+// 				<>
+// 					{languages
+// 						.filter(item => item !== currentLang)
+// 						.map(item => (
+// 							<button
+// 								key={item}
+// 								data-name={item}
+// 								style={{
+// 									backgroundImage: `url(${imageLang[item]})`
+// 								}}
+// 								className='button__lang button__lang-select'
+// 								onClick={onMouseUp}
+// 							/>
+// 						))}
+// 				</>
+// 			)}
+
+// 			<button
+// 				ref={currentBtnRef}
+// 				data-name='current'
+// 				style={{
+// 					backgroundImage: `url(${imageLang[currentLang]})`
+// 				}}
+// 				className='button__lang'
+// 				onClick={onMouseUp}
+// 			/>
+// 		</div>
+// 	)
+// }
+
+// export default LanguageSelect
