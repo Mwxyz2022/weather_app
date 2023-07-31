27337e6bb34e4e8c331b5ca2fe46e3bc098c6a82
Mwxyz2022 <rozhukvlad@gmail.com>
2023-07-31 16:18:40 +0300

    fixed

--- src/components/main/cards/cities-nav/CitiesNavList.tsx
+++ src/components/main/cards/cities-nav/CitiesNavList.tsx
@@ -11,11 +11,11 @@ import { AppContextValue } from '../../../../types/types'
 import './cities-navigation.css'
 
 const CitiesNavList: FC = () => {
-	const screenWidth = useScreenWidth()
 	const [isOpenList, setIsOpenList] = useState<boolean>(false)
 	const { storedCities, favoriteCities } = useContext<AppContextValue>(AppContext)
 	const { cityId } = useParams()
 
+	const screenWidth = useScreenWidth()
 	const isMobileScreen = screenWidth < 750
 
 	return (
@@ -23,31 +23,57 @@ const CitiesNavList: FC = () => {
 			{!!storedCities.length &&
 				storedCities.map((city: ICityData) => {
 					const isActive = cityId === `${city.id}`
-
 					const isFavoriteCity = favoriteCities.some(favCity => favCity.id === city.id)
+
+					const getDisplay = () => {
+						if (isMobileScreen) return isOpenList || isActive ? 'flex' : 'none'
+
+						return 'flex'
+					}
+
+					const getBorderRadius = () => {
+						if (
+							(isMobileScreen && !isOpenList && isActive) ||
+							(isMobileScreen && storedCities.length === 1)
+						)
+							return '4px'
+
+						if (!isMobileScreen) return '4px 4px 0 0'
+
+						return ''
+					}
+
+					const onListCityHandler = () => {
+						if (isMobileScreen && storedCities.length > 1) {
+							setIsOpenList(prev => !prev)
+						}
+						return
+					}
 
 					return (
 						<li
 							key={city.id}
 							className={isActive ? 'city-nav-item link-active' : 'city-nav-item'}
 							style={{
-								display: isOpenList || isActive ? 'flex' : 'none',
-
-								borderRadius: isMobileScreen && !isOpenList && isActive ? '4px' : ''
+								display: getDisplay(),
+								borderRadius: getBorderRadius()
 							}}
-							onClick={() => setIsOpenList(prev => !prev)}
+							onClick={onListCityHandler}
 						>
 							<Link className='city-link' to={`/city/${city.id}`}>
+								{city.name}
 								{isFavoriteCity && (
 									<AiTwotoneStar
 										size={15}
-										style={{ color: 'orange', paddingTop: 2, marginRight: 4 }}
+										style={{ color: 'orange', paddingTop: 2, marginLeft: 4 }}
 									/>
 								)}
-								{city.name}
 
 								{isMobileScreen && !isOpenList && isActive ? (
-									<VscListFlat style={{ marginLeft: 8 }} />
+									<span className='list__icon'>
+										<VscListFlat style={{ marginLeft: 8 }} />
+										<span className='icon__counter'>{storedCities.length}/5</span>
+									</span>
 								) : null}
 							</Link>
 						</li>
